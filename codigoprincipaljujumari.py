7550740fecb8e729636b941bccf292a4e99be6a7
mariana4lb <161875797+mariana4lb@users.noreply.github.com>
2024-05-24 16:26:48 -0300

    Update codigoprincipaljujumari.py

--- codigoprincipaljujumari.py
+++ codigoprincipaljujumari.py
@@ -12,8 +12,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
 
+Window.size = 360,640
 
+#Julia e Mariana
 class Home(BoxLayout):
     def __init__(self, **kwargs):
         super(Home, self).__init__(**kwargs)
@@ -24,7 +27,7 @@ class Home(BoxLayout):
         self.spacing = 10
 
         # Adicione a imagem (certifique-se de que o arquivo esteja no mesmo diretório)
-        self.add_widget(Image(source='IMG_9831-removebg-preview.png', size=(200, 200)))
+        self.add_widget(Image(source='IMG_9831-removebg-preview.png', size_hint=(None,None),size=(250,250)))
         self.add_widget(Label(text="ChefeVirtual", font_size=50, font_name='Impact', color=get_color_from_hex('#d5fff4')))
         self.add_widget(Label(text="Seja Bem-Vinda", font_size=20, font_name='Arial', color=get_color_from_hex('#d5fff4')))
 
@@ -103,70 +106,56 @@ class TelaCadastro(BoxLayout):
 
     def tela_pratos(self, *args):
         self.parent.parent.current = 'Pratos'
+
+from kivy.uix.gridlayout import GridLayout
+
 class TelaPratos(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Window.clearcolor = get_color_from_hex("#ff8e31")
         self.cols = 2
-        self.size_hint = (1, 1)
-        self.title_label = Label(text="Menu", halign="right", valign="top", size_hint=(0.5, 0.5), height="40dp", color='#722f37', font_size="25sp", pos_hint={20, 20})
-        self.add_widget(self.title_label)
-        
 
-
-        # Adicione as imagens e botões
-        self.image_layout = GridLayout(cols=2)
-        self.add_widget(self.image_layout)
-        self.add_pratos_de_entrada_image_and_button()
-        self.add_veganos_image_and_button()
-        self.add_sobremesa_image_and_button()
-        self.add_aperitivo_image_and_button()
-
-        self.button = Button(text=" adicionar uma receita", size_hint=(0, 0.1), background_color=('#9E0A0A'), color=(1, 1, 1, 1), pos_hint={'x': 0.25, 'y': 1})
-        self.button.bind(on_release=self.on_button_release)
-        self.add_widget(self.button)
-        
-    def add_pratos_de_entrada_image_and_button(self):
-        self.image1 = Image(source=r'C:\Users\aluno.sesipaulista\Desktop\aplicativo-de-receitas-\imagens e icones\1.jpeg', size_hint=(1, None), height='200dp')
-        self.button1 = Button(text='entrada', size_hint=(0.5, None), halign="center", valign="center", height='30dp', background_color=('#9E0A0A'), color=(1, 1, 1, 1), pos_hint={'x': 0.25, 'top': 1})
+        # Bloco Entrada
+        entrada_layout = GridLayout(cols=1, size_hint_y=None, height='230dp')
+        self.image1 = Image(source='1.jpeg', size_hint_y=None, height='200dp')
+        self.button1 = Button(text='entrada', size_hint_y=None, height='30dp', background_color=(0.619, 0.039, 0.039, 1))
         self.button1.bind(on_release=self.on_button_release)
-        self.layout1 = BoxLayout(orientation='vertical')
-        self.layout1.add_widget(self.image1)
-        self.layout1.add_widget(self.button1)
-        self.add_widget(self.layout1)
-        self.layout1.pos_hint = {'top': 1}
+        entrada_layout.add_widget(self.image1)
+        entrada_layout.add_widget(self.button1)
 
-    def add_veganos_image_and_button(self):
-        self.image2 = Image(source=r'C:\Users\aluno.sesipaulista\Desktop\aplicativo-de-receitas-\imagens e icones\2.jpeg', size_hint=(1, None), height='200dp')
-        self.button2 = Button(text='veganos', size_hint=(0.5, None), height='30dp', background_color=('#9E0A0A'), color=(1, 1, 1, 1), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        self.layout2 = BoxLayout(orientation='vertical')
+        # Bloco Veganos
+        veganos_layout = GridLayout(cols=1, size_hint_y=None, height='230dp')
+        self.image2 = Image(source='2.jpeg', size_hint_y=None, height='200dp')
+        self.button2 = Button(text='veganos', size_hint_y=None, height='30dp', background_color=(0.619, 0.039, 0.039, 1))
         self.button2.bind(on_release=self.on_button_release)
-        self.layout2.add_widget(self.image2)
-        self.layout2.add_widget(self.button2)
-        self.add_widget(self.layout2)
-        
-    def add_sobremesa_image_and_button(self):
-        self.image3 = Image(source=r'C:\Users\aluno.sesipaulista\Desktop\aplicativo-de-receitas-\imagens e icones\3.jpeg', size_hint=(1, None), height='200dp')
-        self.button3= Button(text='sobremesas', size_hint=(0.5, None), height='30dp', background_color=('#9E0A0A'), pos_hint={'x': 0.25, 'self_y': 0.5})
-        self.layout3 = BoxLayout(orientation='vertical')
+        veganos_layout.add_widget(self.image2)
+        veganos_layout.add_widget(self.button2)
+
+        # Adicionando os layouts de Entrada e Veganos ao layout principal
+        self.add_widget(entrada_layout)
+        self.add_widget(veganos_layout)
+
+        # Bloco Sobremesas
+        sobremesas_layout = GridLayout(cols=1, size_hint_y=None, height='230dp')
+        self.image3 = Image(source='3.jpeg', size_hint_y=None, height='200dp')
+        self.button3 = Button(text='sobremesas', size_hint_y=None, height='30dp', background_color=(0.619, 0.039, 0.039, 1))
         self.button3.bind(on_release=self.on_button_release)
-        self.layout3.add_widget(self.image3)
-        self.layout3.add_widget(self.button3)
-        self.add_widget(self.layout3)
+        sobremesas_layout.add_widget(self.image3)
+        sobremesas_layout.add_widget(self.button3)
+        self.add_widget(sobremesas_layout)
 
-        
-    def add_aperitivo_image_and_button(self):
-        self.image4 = Image(source=r'C:\Users\aluno.sesipaulista\Desktop\aplicativo-de-receitas-\imagens e icones\4.jpeg', size_hint=(1, None), height='200dp')
-        self.button4 = Button(text='aperitivos', size_hint=(0.5, None), height='30dp', background_color=('#9E0A0A'), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        # Bloco Aperitivos
+        aperitivos_layout = GridLayout(cols=1, size_hint_y=None, height='230dp')
+        self.image4 = Image(source='4.jpeg', size_hint_y=None, height='200dp')
+        self.button4 = Button(text='aperitivos', size_hint_y=None, height='30dp', background_color=(0.619, 0.039, 0.039, 1))
         self.button4.bind(on_release=self.on_button_release)
-        self.layout4 = BoxLayout(orientation='vertical')
-        self.layout4.add_widget(self.image4)
-        self.layout4.add_widget(self.button4)
-        self.add_widget(self.layout4)
-
+        aperitivos_layout.add_widget(self.image4)
+        aperitivos_layout.add_widget(self.button4)
+        self.add_widget(aperitivos_layout)
 
     def on_button_release(self, instance):
-        print('Botão liberado')
+        print("Button released")
+
 
 
 class MyApp(App):
@@ -197,3 +186,4 @@ class MyApp(App):
 
 if __name__ == '__main__':
      MyApp().run()
+   
