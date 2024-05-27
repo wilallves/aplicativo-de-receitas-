c4d767697bde1297aa3d4933f045f23dc55fdb79
wil ꩜ <78051450+wilallves@users.noreply.github.com>
2024-05-27 16:42:46 -0300

    receitapudim.py

--- receitapudim.py
+++ receitapudim.py
@@ -33,15 +33,11 @@ class RecipeApp(App):
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
         ingredients = [
-            '1- 500g de peito de frango cortado em cubos',
-            '2- 1 cebola picada',
-            '3- 2 dentes de alho picados',
-            '4- 1 colher de sopa de óleo vegetal',
-            '5- 1 lata de creme de leite',
-            '6- 1 lata de molho de tomate',
-            '7- 1/2 xícara de ketchup',
-            '8- Sal e pimenta a gosto',
-            'Arroz branco para acompanhar',
+            '1- Leite condensado (1 lata)',
+            '2- Leite (2 medidas da lata de leite condensado)',
+            '3- Ovos (3 unidades)',
+            '4- Açúcar (para caramelizar a forma)',
+        
         ]
 
         ingredient_label = Label(text='Ingredientes:\n' + '\n'.join(ingredients), size_hint_y=400, height=300, color=[1,1,1,1])
@@ -50,7 +46,7 @@ class RecipeApp(App):
         top_layout = BoxLayout(orientation='horizontal', size_hint_y=100, height=500, size_hint_x=1.5)
         bottom_layout = BoxLayout(orientation='vertical', size_hint_y=None, height=400)
 
-        foto_lasanha = Image(source='pudim.jpg', size_hint= (2, 1))
+        foto_lasanha = Image(source='pudindin.jpg', size_hint= (2, 1))
         input_receita = TextInput (
             hint_text='Adicione um comentário', 
             size_hint_y=50, height=50, size_hint_x=0.9,
@@ -83,32 +79,21 @@ class RecipeApp(App):
             
 
             """
-            suponha o açúcar e a água em uma fôrma de pudim grande (28 cm) e misture até o açúcar dissolver.
-
-Leve ao fogo baixo e deixe cozinhar, sem mexer, por 10 minutos, ou até o caramelo estar marrom. Retire do fogo e com o auxílio das costas de uma colher, espalhe o caramelo por toda a fôrma. Reserve.
-
-Preaqueça o forno em temperatura média (180° C).
-
-No copo do liquidificar, coloque o leite condensado, o leite, os ovos e o amido de milho MAIZENA . Bata até obter uma mistura homogênea.
-
-Transfira a mistura à fôrma de pudim já caramelizada e cubra-a com papel-alumínio.
-
-Coloque a fôrma de pudim dentro de uma assadeira e leve ao forno em banho-maria, por 1 hora, ou até que o pudim esteja assado.
-
-Retire do forno, tire a fôrma de pudim de dentro da assadeira e deixe esfriar. Leve à geladeira, por no mínimo 2 horas.
+            Para preparar um delicioso pudim de leite condensado, comece caramelizando uma forma para pudim. Em seguida, bata no liquidificador uma lata de leite condensado, duas medidas de leite e três ovos até ficar homogêneo. Despeje a mistura na forma caramelizada e asse em banho-maria a 180°C por cerca de 1 hora e 30 minutos, até que o pudim esteja firme. Após esfriar, desenforme e sirva gelado ou à temperatura ambiente. Se desejar, decore com calda de caramelo ou frutas. Aproveite!.
 """
         )
 
-        preparation_label = Label(text='Modo de Preparo:\n' + preparation,font_size=12, size_hint_y=None, height=400, color=[1,1,1,1])
+        preparation_label = Label(text='Modo de Preparo:\n' + preparation,font_size=18, size_hint_y=None, height=400, color=[1,1,1,1])
         top_layout = self.main_layout.children[1]
         bottom_layout = self.main_layout.children[0]
 
         top_layout.clear_widgets()
         bottom_layout.clear_widgets()
 
-        foto_lasanha = Image(source='pudim.jpg', size_hint= (2, 1))
+        foto_lasanha = Image(source='pudindin.jpg', size_hint= (2, 1))
         
         bottom_layout.add_widget(preparation_label)
         top_layout.add_widget(foto_lasanha)
         top_layout.add_widget(BoxLayout())
-      
+if __name__ == "__main__":
+    RecipeApp().run()
