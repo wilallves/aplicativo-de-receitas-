edee3be0612f0726c1a9746358390e27e24b778e
wil ꩜ <78051450+wilallves@users.noreply.github.com>
2024-06-10 17:09:21 -0300

    Add files via upload

---
+++ novos.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 10/06/2024 às 22:12
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `usuários`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `novos`
+--
+
+CREATE TABLE `novos` (
+  `nome` varchar(50) NOT NULL,
+  `e-mail` varchar(50) NOT NULL,
+  `senha` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `novos`
+--
+
+INSERT INTO `novos` (`nome`, `e-mail`, `senha`) VALUES
+('wildson', 'wildson@gmail.com', '12345'),
+('júlia', 'julia@gmail.com', '12345');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
